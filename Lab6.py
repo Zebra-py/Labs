5f8130e2d1e209a923439c008f2f4e4e5294259f
Kelly <kelasev2@mail.ru>
2023-10-24 02:29:21 +0300

    A

--- Lab6.py
+++ Lab6.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+def get_records(table, field):
+    conn = sqlite3.connect('goods.db')  #Взял какую-то бд из интернета для проверки
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM {table} WHERE {field} IS NOT NULL"
+    cursor.execute(query)
+
+    records = cursor.fetchall()
+
+    conn.close()
+
+    return records
+
+table = 'products'
+field = 'product_name'
+
+result = get_records(table, field)
+
+for record in result:
+    print(record)
